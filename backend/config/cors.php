3682e2125e3e36fb97fedccfdcd71a77fe0c551a
Gaufo Gomer Jr <gaufogomer@gmail.com>
2025-09-16 22:58:13 +0800

    fix: cors error

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,7 +1,16 @@
 <?php
 session_name("ReactPHPStarter");
 
-header("Access-Control-Allow-Origin: http://localhost:5174");
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header("Access-Control-Allow-Origin: http://localhost:5173");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+    header("Access-Control-Allow-Credentials: true");
+    http_response_code(204); // No content
+    exit;
+}
+
+header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Credentials: true");
